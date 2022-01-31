47bbd145227520daa477ba0bb15c14de7ec61ddd
SeanBoden <seanboden13@gmail.com>
2022-01-31 17:11:06 -0600

    Created SemesterController for web endpoints that allow Admins to perform CRUD operations on the Semesters table in the database. The endpoint with this commit is GetSemesters which returns a list of all semesters in the table.

---
+++ AbetApi/Controllers/SemesterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbetApi.EFModels;
+using AbetApi.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AbetApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SemesterController : ControllerBase
+    {
+        //[Authorize(Roles = RoleTypes.Admin)]
+        [HttpGet("GetSemesters")]
+        public List<Semester> GetSemesters()
+        {
+            return Semester.GetSemesters();
+        }
+}
