c76a206fca768891b4c9c072ae3e3791a4668fd1
SCBishop <stephencorybishop@gmail.com>
2022-02-01 12:30:29 -0600

    Finished true test cases for Users. Will revise tests to be more thorough once error handling is implemented.

--- AbetApi.Tests/AbetApi.Tests.cs
+++ AbetApi.Tests/AbetApi.Tests.cs
@@ -1,28 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http.Results;
+using AbetApi.Data;
+using AbetApi.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AbetApi.EFModels;
-using AbetApi.Data;
-
-
+using System.Collections.Generic;
 
 namespace AbetApi.Tests
 {
     [TestClass]
     public class AbetApiTests
     {
+        /* Test(s) for the User methods */
+
+        public void AddUserHelper(User __user)
+        {
+            User.AddUser(__user);
+        }
+
+        public void RemoveUserHelper(string __euid)
+        {
+            User.DeleteUser(__euid);
+        }
+
+        public void EditUserHelper(string __euid, User __newuserinfo)
+        {
+            User.EditUser(__euid, __newuserinfo);
+        }
+
+        public User GetUserHelper(string __euid)
+        {
+            return User.GetUser(__euid).Result;
+        }
+
         [TestMethod]
         public void TestAddUser()
         {
-            // Instantiate a user, in local memory
-            var __user = new User("Cory", "Bishop", "scb0231");
-            // Write user to DB
-            User.AddUser(__user);
-            // Grab the user's information via EUID
-            var __expectedresult = User.GetUser("scb0231").Result;
-            // Check attributes vs one another
+            var __user = new User("Gandalf", "Grey", "gtg001");
+            AddUserHelper(__user);
+            var __expectedresult = GetUserHelper(__user.EUID);
             Assert.AreEqual(__user.EUID, __expectedresult.EUID);
             Assert.AreEqual(__user.FirstName, __expectedresult.FirstName);
             Assert.AreEqual(__user.LastName, __expectedresult.LastName);
@@ -31,16 +48,26 @@ namespace AbetApi.Tests
         [TestMethod]
         public void TestDeleteUser()
         {
-            // Delete a user we know exists
-            // Need to handle case where we delete a user who doesn't exist in DB
-            // This test case going to fail until we flesh out method a little more
-            var __user = new User("John", "Connor", "terminator");
-            User.AddUser(__user); 
-            var __euidtodelete = "terminator";
-            User.DeleteUser(__euidtodelete);
-            var __expectedresult = User.GetUser("terminator");
-            Assert.AreEqual(__expectedresult.Result, null);
+            var __user = new User("Samwise", "Gamgee", "ssg001");
+            User.AddUser(__user);
+            var __euidtodelete = "ssg001";
+            RemoveUserHelper(__euidtodelete);
+            var __expectedresult = GetUserHelper(__user.EUID);
+            Assert.AreEqual(__expectedresult, null);
         }
 
+        [TestMethod]
+        public void TestEditUser()
+        {
+            var __euidtoedit = "ssg001";
+            var __user = new User("Frodo", "Baggins", "fb0001");
+            AddUserHelper(__user);
+            EditUserHelper(__euidtoedit, __user);
+            var __expectedresult = GetUserHelper(__user.EUID);
+            Assert.AreEqual(__user.FirstName, __expectedresult.FirstName);
+            Assert.AreEqual(__user.LastName, __expectedresult.LastName);
+            Assert.AreEqual(__user.EUID, __expectedresult.EUID);
+            RemoveUserHelper("TestID");
+        }
     }
 }
