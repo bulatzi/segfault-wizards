19fda53bb841e1e5295d309c7509d4226eb7f22c
Bryan <Bulatzi@gmail.com>
2022-02-02 13:43:00 -0600

    Re-adds the authorize tags to majors endpoints

--- AbetApi/Controllers/MajorController.cs
+++ AbetApi/Controllers/MajorController.cs
@@ -13,29 +13,28 @@ namespace AbetApi.Controllers
     [Route("[controller]")]
     public class MajorController : ControllerBase
     {
-        //[Authorize(Roles = RoleTypes.Admin)]
+        [Authorize(Roles = RoleTypes.Admin)]
         [HttpPost("AddMajor")]
         public void AddMajor(string term, int year, string name)
         {
             Major.AddMajor(term, year, name);
         }
 
-        //[Authorize(Roles = RoleTypes.Admin)]
+        [Authorize(Roles = RoleTypes.Admin)]
         [HttpGet("GetMajors")]
         public List<Major> GetMajors(string term, int year)
         {
             return Major.GetMajors(term, year);
         }
 
-
-        //[Authorize(Roles = RoleTypes.Admin)]
+        [Authorize(Roles = RoleTypes.Admin)]
         [HttpPatch("EditMajor")]
         public void EditMajor(string term, int year, string name, string NewValue)
         {
             Major.EditMajor(term, year, name, NewValue);
         }
 
-        //[Authorize(Roles = RoleTypes.Admin)]
+        [Authorize(Roles = RoleTypes.Admin)]
         [HttpDelete("DeleteMajor")]
         public void DeleteMajor(string term, int year, string name)
         {
