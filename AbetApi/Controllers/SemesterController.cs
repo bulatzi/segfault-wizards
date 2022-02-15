7ea41c2d3b157f9074cb4a747f6c70739f05771f
ChetL <chet.lockwood@gmail.com>
2022-02-15 02:21:54 -0600

    Added Exception Handling to Semesters
    Added exception handling to the function listed in the controller, did not touch the other functions that are referenced elsewhere

--- AbetApi/Controllers/SemesterController.cs
+++ AbetApi/Controllers/SemesterController.cs
@@ -9,32 +9,55 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace AbetApi.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class SemesterController : ControllerBase
-    {
-        //This function gets all semesters from the Semesters table in the database.
-        [Authorize(Roles = RoleTypes.Admin)]
-        [HttpGet("GetSemesters")]
-        public List<Semester> GetSemesters()
-        {
-            return Semester.GetSemesters();
-        }
+     [ApiController]
+     [Route("[controller]")]
+     public class SemesterController : ControllerBase
+     {
+          //This function gets all semesters from the Semesters table in the database.
+          //[Authorize(Roles = RoleTypes.Admin)]
+          [HttpGet("GetSemesters")]
+          public async Task<IActionResult> GetSemesters()
+          {
+               try
+               {
+                    return Ok(await Semester.GetSemesters());
+               }
+               catch (Exception ex)
+               {
+                    return BadRequest(ex.Message);
+               }
+          }
 
-        //This function adds a semester with the provided information to the database.
-        [Authorize(Roles = RoleTypes.Admin)]
-        [HttpPost("AddSemester")]
-        public void AddSemester(Semester semester)
-        {
-            EFModels.Semester.AddSemester(semester);
-        }
+          //This function adds a semester with the provided information to the database.
+          //[Authorize(Roles = RoleTypes.Admin)]
+          [HttpPost("AddSemester")]
+          public async Task<IActionResult> AddSemester(Semester semester)
+          {
+               try
+               {
+                    await Semester.AddSemester(semester);
+                    return Ok();
+               }
+               catch (Exception ex)
+               {
+                    return BadRequest(ex.Message);
+               } 
+          }
 
-        //This function deletes a semester from the database with the provided information.
-        [Authorize(Roles = RoleTypes.Admin)]
-        [HttpDelete("DeleteSemester")]
-        public void DeleteSemester(Semester semester)
-        {
-            EFModels.Semester.DeleteSemester(semester.Term, semester.Year);
-        }
-    }
+          //This function deletes a semester from the database with the provided information.
+          //[Authorize(Roles = RoleTypes.Admin)]
+          [HttpDelete("DeleteSemester")]
+          public async Task<IActionResult> DeleteSemester(Semester semester)
+          {
+               try
+               {
+                    await Semester.DeleteSemester(semester.Term, semester.Year);
+                    return Ok();
+               }
+               catch (Exception ex)
+               {
+                    return BadRequest(ex.Message);
+               }   
+          }
+     }
 }
--- AbetApi/EFModels/Semester.cs
+++ AbetApi/EFModels/Semester.cs
@@ -7,121 +7,128 @@ using AbetApi.Data;
 
 namespace AbetApi.EFModels
 {
-    public class Semester
-    {
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public int SemesterId { get; set; }
-        public int Year { get; set; }
-        public string Term { get; set; }
-        //string Session { get; set; } // Session is not required. All courses fall under the purview of spring/fall.
-        [JsonIgnore]
-        public ICollection<Course> Courses { get; set; }
-        [JsonIgnore]
-        public ICollection<Major> Majors { get; set; }
+     public class Semester
+     {
+          [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+          public int SemesterId { get; set; }
+          public int Year { get; set; }
+          public string Term { get; set; }
+          //string Session { get; set; } // Session is not required. All courses fall under the purview of spring/fall.
+          [JsonIgnore]
+          public ICollection<Course> Courses { get; set; }
+          [JsonIgnore]
+          public ICollection<Major> Majors { get; set; }
 
-        // This constructor creates an empty semester.
-        // You must add majors and courses afterwards
-        public Semester(string Term, int year)
-        {
-            this.Term = Term;
-            this.Year = year;
-        }
+          // This constructor creates an empty semester.
+          // You must add majors and courses afterwards
+          public Semester(string Term, int year)
+          {
+               this.Term = Term;
+               this.Year = year;
+          }
 
-        Semester() {
-            this.Courses = new List<Course>();
-            this.Majors = new List<Major>();
-        }
+          Semester()
+          {
+               this.Courses = new List<Course>();
+               this.Majors = new List<Major>();
+          }
 
-        public async static void AddSemester(Semester semester)
-        {
-            // Sets the user id to be 0, so entity framework will give it a primary key
-            semester.SemesterId = 0;
+          // NEEDS ADDITIONAL NULL CHECKING AND INPUT SCRUBBING
+          public async static Task AddSemester(Semester semester)
+          {
+               // Sets the user id to be 0, so entity framework will give it a primary key
+               semester.SemesterId = 0;
 
-            //Opens a context with the database, makes changes, and saves the changes
-            await using (var context = new ABETDBContext())
-            {
-                context.Semesters.Add(semester);
-                context.SaveChanges();
-            }
-        }
+               //Opens a context with the database, makes changes, and saves the changes
+               await using (var context = new ABETDBContext())
+               {
+                    context.Semesters.Add(semester);
+                    context.SaveChanges();
 
-        // This function searches for an item with the provided term and year. It returns null if the item isn't found.
-        public async static Task<Semester> GetSemester(string term, int year)
-        {
-            await using (var context = new ABETDBContext()) 
-            {
-                Semester semester = context.Semesters.FirstOrDefault(p => p.Term == term && p.Year == year);
-                return semester;
-            }
-        }
+                    return;
+               }
+          } // AddSemester
 
-        //This function returns all semesters in the Semester table from the database.
-        public static List<Semester> GetSemesters()
-        {
-            using (var context = new ABETDBContext())
-            {
-                return context.Semesters.ToList();
-            }
-        }
+          // This function searches for an item with the provided term and year. It returns null if the item isn't found.
+          public async static Task<Semester> GetSemester(string term, int year)
+          {
+               await using (var context = new ABETDBContext())
+               {
+                    Semester semester = context.Semesters.FirstOrDefault(p => p.Term == term && p.Year == year);
+                    return semester;
+               }
+          }
 
-        // This function finds a semester by term and year, and updates the semester to the values of the provided semester object.
-        // Note: This function may need to also include editing courses and majors in the future, but it's not here currently that data will need to have its own dedicated functions for editing.
-        public async static void EditSemester(string term, int year, Semester NewValue) { 
-            await using (var context = new ABETDBContext()) 
-            {
-                Semester semester = context.Semesters.FirstOrDefault(p => p.Term == term && p.Year == year);
-                semester.Term = NewValue.Term;
-                semester.Year = NewValue.Year;
+          //This function returns all semesters in the Semester table from the database.
+          public static async Task<List<Semester>> GetSemesters()
+          {
+               await using (var context = new ABETDBContext())
+               {
+                    return context.Semesters.ToList();
+               }
+          } // GetSemesters
 
-                context.SaveChanges();
-            }
-        }
+          // This function finds a semester by term and year, and updates the semester to the values of the provided semester object.
+          // Note: This function may need to also include editing courses and majors in the future, but it's not here currently that data will need to have its own dedicated functions for editing.
+          public async static void EditSemester(string term, int year, Semester NewValue)
+          {
+               await using (var context = new ABETDBContext())
+               {
+                    Semester semester = context.Semesters.FirstOrDefault(p => p.Term == term && p.Year == year);
+                    semester.Term = NewValue.Term;
+                    semester.Year = NewValue.Year;
 
-        // This function finds a semester and deletes it.
-        // Anybody calling this function should make sure you want to call this function. Deletions are final.
-        public async static void DeleteSemester(string term, int year)
-        {
-            await using (var context = new ABETDBContext())
-            {
-                Semester semester = context.Semesters.FirstOrDefault(p => p.Term == term && p.Year == year);
-                context.Remove(semester);
-                context.SaveChanges();
-            }
-        }
+                    context.SaveChanges();
+               }
+          }
 
-        public static List<Course> GetCourses(string term, int year)
-        {
-            List<Course> list = new List<Course>();
+          // This function finds a semester and deletes it.
+          // Anybody calling this function should make sure you want to call this function. Deletions are final.
+          public async static Task DeleteSemester(string term, int year)
+          {
+               await using (var context = new ABETDBContext())
+               {
+                    Semester semester = context.Semesters.FirstOrDefault(p => p.Term == term && p.Year == year);
+                    context.Remove(semester);
+                    context.SaveChanges();
 
-            using(var context = new ABETDBContext())
-            {
-                //FIXME - Add null check
-                Semester semester = context.Semesters.FirstOrDefault(p => p.Term == term && p.Year == year);
-                context.Entry(semester).Collection(semester => semester.Courses).Load();
+                    return;
+               }
+          } // DeleteSemester
 
-                foreach(var course in semester.Courses)
-                {
-                    list.Add(course);
-                }
-                return list;
-            }
-        }
-    }
-    /*
-        As of November 17th, 2021, the current semester catalogue is:
-            Fall 2021
-                8W1 Session
-                8W2 Session
-            Spring 2022
-                3W1 Session  (Side note: This is the winter semester)
-                8W1 Session
-                8W2 Session
-            Summer 2022
-                3W1 Session
-                8W1 Session
-                8W2 Session
-                5W1 Session
-                10W Session
-                5W2 Session
-     */
+          public static List<Course> GetCourses(string term, int year)
+          {
+               List<Course> list = new List<Course>();
+
+               using (var context = new ABETDBContext())
+               {
+                    //FIXME - Add null check
+                    Semester semester = context.Semesters.FirstOrDefault(p => p.Term == term && p.Year == year);
+                    context.Entry(semester).Collection(semester => semester.Courses).Load();
+
+                    foreach (var course in semester.Courses)
+                    {
+                         list.Add(course);
+                    }
+                    return list;
+               }
+          }
+     }
+     /*
+         As of November 17th, 2021, the current semester catalogue is:
+             Fall 2021
+                 8W1 Session
+                 8W2 Session
+             Spring 2022
+                 3W1 Session  (Side note: This is the winter semester)
+                 8W1 Session
+                 8W2 Session
+             Summer 2022
+                 3W1 Session
+                 8W1 Session
+                 8W2 Session
+                 5W1 Session
+                 10W Session
+                 5W2 Session
+      */
 }
