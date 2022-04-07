b626f6a40ef10dbc3351a7fc5afab1598c6366dc
bulatzi <bulatzi@gmail.com>
2022-04-06 20:18:53 -0500

    Creates a report controller, and adds a temporary version of the GenerateFullReport endpoint.

---
+++ AbetApi/Controllers/ReportController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AbetApi.EFModels;
+using AbetApi.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+namespace AbetApi.Controllers
+{
+    public class ReportController : ControllerBase
+    {
+        //[Authorize(Roles = RoleTypes.Instructor)]
+        [HttpGet("GenerateFullReport")]
+        public async Task<IActionResult> GenerateFullReport(string term, int year, string major)
+        {
+            try
+            {
+
+                //Converts the data to a format for the front end and returns the data
+                return Ok(AbetApi.Models.FullReport.ConvertToModelFullReport());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        } // GetGrades
+    }
+}
---
+++ AbetApi/Models/FullReport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace AbetApi.Models
+{
+    public class FullReport
+    {
+        public static Dictionary<string, int[]> ConvertToModelFullReport()
+        {
+            //List<Dictionary<string, int[]>> tempList = new List<Dictionary<string, int[]>>();
+
+            Dictionary<string, int[]> tempdictionary = new Dictionary<string, int[]>();
+
+            int[] tempArray = new int[] { 0,5,0,0,1,0,0,1,0,1};
+
+            tempdictionary.Add("1010", tempArray);
+            tempdictionary.Add("1030", tempArray);
+            tempdictionary.Add("2010", tempArray);
+            tempdictionary.Add("2030", tempArray);
+
+            return tempdictionary;
+        }
+    }
+}
