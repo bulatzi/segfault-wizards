5d4117eefbe4e69b53da9f2cd0f24b29bc9ae4a8
bulatzi <bulatzi@gmail.com>
2022-04-06 19:55:48 -0500

    Adds a work in progress version of the StudentOutcomesCompleted Controller endpoints

---
+++ AbetApi/Controllers/StudentOutcomesCompletedController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AbetApi.EFModels;
+using AbetApi.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+namespace AbetApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentOutcomesCompletedController : ControllerBase
+    {
+        //[Authorize(Roles = RoleTypes.Instructor)]
+        [HttpGet("GetStudentOutcomesCompleted")]
+        public async Task<IActionResult> GetStudentOutcomesCompleted(string term, int year, string department, string courseNumber, string sectionNumber)
+        {
+            try
+            {
+                //Get the specified grades
+                //var grades = await Grade.GetGrades(term, year, department, courseNumber, sectionNumber);
+
+                //Converts the data to a format for the front end and returns the data
+                //return Ok(AbetApi.Models.Grade.ConvertToModelGrade(grades));
+                return Ok(AbetApi.Models.StudentOutcomesCompleted.ConvertToModelStudentOutcomesCompleted());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        } // GetStudentOutcomesCompleted
+
+        //[Authorize(Roles = RoleTypes.Instructor)]
+        [HttpPost("SetStudentOutcomesCompleted")]
+        public async Task<IActionResult> SetStudentOutcomesCompleted(string term, int year, string department, string courseNumber, string sectionNumber, Dictionary<string, string> studentOutcomesCompletedDictionary)
+        {
+            try
+            {
+                //await Grade.SetGrades(term, year, department, courseNumber, sectionNumber, AbetApi.Models.Grade.ConvertToEFModelGrade(gradesDictionary));
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        } // SetStudentOutcomesCompleted
+    }
+}
---
+++ AbetApi/Models/StudentOutcomesCompleted.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace AbetApi.Models
+{
+    public class StudentOutcomesCompleted
+    {
+
+        public static List<Dictionary<string, string>> ConvertToModelStudentOutcomesCompleted()
+        {
+            List<Dictionary<string, string>> tempList = new List<Dictionary<string, string>>();
+
+            Dictionary<string, string> tempDictionary = new Dictionary<string, string>();
+
+            tempDictionary.Add("outcomeName", "1");
+            tempDictionary.Add("outcomeDescription", "Describe how a computer's CPU, Main Memory, Secondary Storage and I/O work together to execute a computer program.");
+            tempDictionary.Add("IT", "20");
+            tempDictionary.Add("CS", "30");
+            tempDictionary.Add("CE", "40");
+            tempDictionary.Add("CYS", "10");
+
+            tempList.Add(tempDictionary);
+
+            tempDictionary = new Dictionary<string, string>();
+
+            tempDictionary.Add("outcomeName", "2");
+            tempDictionary.Add("outcomeDescription", "Make use of a computer system's hardware, editor(s), operating system, system software and network to build computer software and submit that software for grading.");
+            tempDictionary.Add("IT", "20");
+            tempDictionary.Add("CS", "30");
+            tempDictionary.Add("CE", "40");
+            tempDictionary.Add("CYS", "10");
+
+            tempList.Add(tempDictionary);
+
+            return tempList;
+        }
+    }
+}
